8318f080d9f7e6c1d58454cdef6f1ea6126012b4
Aybek <turk.883ar@gmail.com>
2023-02-20 00:52:48 -0800

    Make two type for Update and UpdateResponse

--- clients/telegram/types.go
+++ clients/telegram/types.go
@@ -1,3 +1,13 @@
 package telegram
 
 // Here we define all the types that the client will work with
+
+type UpdatesResponse struct {
+	Ok     bool     `json:"ok"`
+	Result []Update `json:"result"`
+}
+
+type Update struct {
+	ID      int    `json:"update_id"`
+	Message string `json:"message"`
+}
