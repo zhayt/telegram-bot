f9616e766ab8631ef3eb1bc2e7c714db00edc2db
Aybek <turk.883ar@gmail.com>
2023-02-20 01:59:54 -0800

    Make two clients methods (Updates and SendMessage) for get and send messages, also func to do request to telegram api

--- clients/telegram/telegram.go
+++ clients/telegram/telegram.go
@@ -1,12 +1,26 @@
 package telegram
 
-import "net/http"
+import (
+	"encoding/json"
+	"github.com/zhayt/read-adviser-bot/lib/e"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+)
 
 type Client struct {
 	host     string
 	basePath string
 	client   http.Client
 }
+
+// telegram api methods
+const (
+	getUpdatesMethod  = "getUpdates"
+	sendMessageMethod = "sendMessage"
+)
 
 func New(host string, token string) Client {
 	return Client{
@@ -21,10 +35,78 @@ func Token(token string) string {
 }
 
 // Updates gets messages
-func (c *Client) Updates() {
+func (c *Client) Updates(offset int, limit int) (updates []Update, err error) {
+	defer func() { err = e.WrapIfErr("can't get updates", err) }()
 
+	// prepare request parameters
+	q := url.Values{}
+	q.Add("offset", strconv.Itoa(offset))
+	q.Add("limit", strconv.Itoa(limit))
+
+	// do request
+	data, err := c.doRequest(getUpdatesMethod, q)
+	if err != nil {
+		return nil, err
+	}
+
+	var res UpdatesResponse
+
+	// parse json
+	if err := json.Unmarshal(data, &res); err != nil {
+		return nil, err
+	}
+
+	return res.Result, nil
 }
 
-func (c *Client) SendMessage() {
+func (c *Client) SendMessage(chatId int, text string) error {
+	// prepare the request parameters
+	q := url.Values{}
+	q.Add("chat_id", strconv.Itoa(chatId))
+	q.Add("text", text)
 
+	// do request
+	_, err := c.doRequest(sendMessageMethod, q)
+	if err != nil {
+		return e.Wrap("can't send message", err)
+	}
+
+	return nil
+}
+
+func (c *Client) doRequest(method string, query url.Values) (data []byte, err error) {
+	defer func() { err = e.WrapIfErr("can't do request", err) }()
+
+	// define url that we send request
+	u := url.URL{
+		Scheme: "https",
+		Host:   c.host,
+		Path:   path.Join(c.basePath, method),
+	}
+
+	// prepare the request object
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// pass the request parameters to our request object
+	req.URL.RawQuery = query.Encode()
+
+	// send request
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// close response body
+	defer func() { _ = resp.Body.Close() }()
+
+	// get the body contents
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
 }
