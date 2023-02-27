9258acabb495ef621ab4f73ce8aa348c6eac1e83
Aybek <turk.883ar@gmail.com>
2023-02-27 08:22:06 -0800

    Update messages.go

---
+++ events/telegram/messages.go
@@ -0,0 +1,18 @@
+package telegram
+
+const msgHelp = `I can sve and keep you pages. Also I can offer you them to read.
+
+In order to save the page, just send me a link to it.
+
+In order to get a random page from your list, send me command /rnd.
+Caution! After that, this page will be removed from your list!
+`
+
+const msgHello = "As-Salamy Aleikym! 🤗\n" + msgHelp
+
+const (
+	msgUnknownCommand = "Unknown command🤔"
+	msgNoSavedPages   = "You have no saved pages🙊"
+	msgSaved          = "Saved!👌"
+	msgAlreadyExists  = "You have already have this page in your list😁"
+)
