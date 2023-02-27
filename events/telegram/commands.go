913a56a10ae92e7e58a8273f2c7e19bb17c21e0d
Aybek <turk.883ar@gmail.com>
2023-02-27 08:20:27 -0800

    make processor methods

---
+++ events/telegram/commands.go
@@ -0,0 +1,112 @@
+package telegram
+
+import (
+	"context"
+	"errors"
+	"github.com/zhayt/read-adviser-bot/clients/telegram"
+	"github.com/zhayt/read-adviser-bot/lib/e"
+	"github.com/zhayt/read-adviser-bot/storage"
+	"log"
+	"net/url"
+	"strings"
+)
+
+const (
+	RndCmd   = "/rnd"
+	HelpCmd  = "/help"
+	StartCmd = "/start"
+)
+
+func (p *Processor) doCmd(text string, chatID int, username string) error {
+	text = strings.TrimSpace(text)
+
+	log.Printf("got new command '%s' from '%s'", text, username)
+
+	if isAddCmd(text) {
+		return p.savePage(chatID, text, username)
+	}
+	switch text {
+	case RndCmd:
+		return p.sendRandom(chatID, username)
+	case HelpCmd:
+		return p.sendHelp(chatID)
+	case StartCmd:
+		return p.sendHello(chatID)
+	default:
+		return p.tg.SendMessage(chatID, msgUnknownCommand)
+	}
+}
+
+func (p *Processor) savePage(chatId int, pageURL string, username string) (err error) {
+	defer func() { err = e.WrapIfErr("can't do command: save page", err) }()
+
+	sendMsg := NewMessageSender(chatId, p.tg)
+
+	page := &storage.Page{
+		URL:      pageURL,
+		UserName: username,
+	}
+
+	isExist, err := p.storage.IsExists(context.Background(), page)
+	if err != nil {
+		return err
+	}
+
+	if isExist {
+		return sendMsg(msgAlreadyExists)
+	}
+
+	if err = p.storage.Save(context.Background(), page); err != nil {
+		return err
+	}
+
+	if err = sendMsg(msgSaved); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *Processor) sendRandom(chatID int, username string) (err error) {
+	defer func() { err = e.WrapIfErr("can't do command: can't send random", err) }()
+
+	sendMsg := NewMessageSender(chatID, p.tg)
+
+	page, err := p.storage.PickRandom(context.Background(), username)
+	if err != nil && !errors.Is(err, storage.ErrNoSavedPages) {
+		return err
+	}
+	if errors.Is(err, storage.ErrNoSavedPages) {
+		return sendMsg(msgNoSavedPages)
+	}
+
+	if err := sendMsg(page.URL); err != nil {
+		return err
+	}
+
+	return p.storage.Remove(context.Background(), page)
+}
+
+func (p *Processor) sendHelp(chatID int) error {
+	return p.tg.SendMessage(chatID, msgHelp)
+}
+
+func (p *Processor) sendHello(chatId int) error {
+	return p.tg.SendMessage(chatId, msgHello)
+}
+
+func NewMessageSender(chatID int, tg *telegram.Client) func(string) error {
+	return func(msg string) error {
+		return tg.SendMessage(chatID, msg)
+	}
+}
+
+func isAddCmd(text string) bool {
+	return isURL(text)
+}
+
+func isURL(text string) bool {
+	u, err := url.Parse(text)
+
+	return err == nil && u.Host != ""
+}
